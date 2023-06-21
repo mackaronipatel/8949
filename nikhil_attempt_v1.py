8ce2ca11eff8fd7d3c6cb25d5349ceac564680b9
Nikhil Thota <nthota92@gmail.com>
2023-06-20 22:24:07 -0700

    we are now iterating through short pages, adding static values, and looping through annotations

--- nikhil_attempt_v1.py
+++ nikhil_attempt_v1.py
@@ -1,6 +1,6 @@
 import os
 import csv
-from pdfrw import PdfReader, PdfDict, PdfWriter
+from pdfrw import PdfReader, PdfDict, PdfWriter, PdfName
 
 
 def read_csv_data(file_path):
@@ -29,17 +29,44 @@ def fill_with_empty_data():
 
 
 def main():
-    csv_file_path = "import_csv.csv"
-    input_pdf_path = "f8949.pdf"
-    output_folder = "output_forms"
+    short_csv = "short.csv"
+    long_csv = "long.csv"
+    template_pdf = "f8949.pdf"
+    output_folder = "output"
 
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    data = read_csv_data(csv_file_path)
-    print(data)
+    short_data = read_csv_data(short_csv)
+    short_pages = (len(short_data) // 14) + 1
 
-    fill_with_empty_data()
+    input_template = PdfReader(template_pdf)
+    template_page = input_template.pages[0]
+
+    new_pdf = PdfWriter()
+    new_pdf_path = "output/output_shell.pdf"
+    for i in range(short_pages):
+        new_pdf.addPage(template_page)
+    new_pdf.write(new_pdf_path)
+
+    new_pdf = PdfReader(new_pdf_path)
+    for page in new_pdf.pages:
+        annotations = page['/Annots']
+        annotations[0].update(PdfDict(V='{}'.format("Mackenzie Patel"), AS='{}'.format("Mackenzie Patel")))
+        annotations[1].update(PdfDict(V='{}'.format("420-69-4200"), AS='{}'.format("420-69-4200")))
+        annotations[4].update(PdfDict(V=PdfName('On'), AS=PdfName('On')))
+        print(annotations[0])
+        for annotation in annotations[5:]:
+            field_name = annotation['/T'][1:-1]
+            field_type = annotation['/FT']
+
+            if field_type == '/Tx':  # Text field
+                random_data = "123"
+                annotation.update(PdfDict(V='{}'.format(random_data), AS='{}'.format(random_data)))
+            else:
+                print("Unknown field type:", field_type)
+
+    PdfWriter().write('output/output_filled.pdf', new_pdf)
 
 
 if __name__ == "__main__":
